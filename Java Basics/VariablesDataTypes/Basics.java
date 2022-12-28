b8fa75d2636d34db54c8926da51eaa8e4628d02b
Turwash Chakraborty <121122397+Turwash@users.noreply.github.com>
2022-12-28 12:43:03 +0530

    Delete Java Basics directory

--- Java Basics/VariablesDataTypes/Basics.java
+++
@@ -1,8 +0,0 @@
-//Boiler code 
-package VariablesDataTypes;
-
-public class Basics {
-    public static void main(String[] args) { //psvm is shortcut 
-        //Main is the first fnc. Run by Compiler 
-    }
-}
